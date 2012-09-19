8324717b7a810219b6e5fd44ca3d8b73e8055055
JD Harrington <jd@jdharrington.net>
2012-09-19 13:55:25 -0400

    run unison with text ui on remote machines

--- .zsh/unison.zsh
+++ .zsh/unison.zsh
@@ -1,1 +1,5 @@
+if [ "$SSH_TTY" != "" ]; then
+  alias unison='unison -ui text'
+fi
+
 alias sync-music='unison -auto -maxthreads 1 "/Volumes/Data/Dropbox/Home/Music/iTunes/iTunes Media/Music" "ssh://hater//Volumes/Data/Dropbox/Home/Music/iTunes/iTunes Media/Music"'

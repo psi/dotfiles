f6ea4ad8839112b6ae5e36a5bdf3888e06523345
JD Harrington <jd@jdharrington.net>
2012-09-20 00:46:00 -0400

    Move unison config into ~/.unison

--- .zsh/unison.zsh
+++ .zsh/unison.zsh
@@ -1,5 +1,2 @@
-if [ "$SSH_TTY" != "" ]; then
-  alias unison='unison -ui text'
-fi
-
-alias sync-music='unison -auto -maxthreads 1 "/Volumes/Data/Dropbox/Home/Music/iTunes/iTunes Media/Music" "ssh://hater//Volumes/Data/Dropbox/Home/Music/iTunes/iTunes Media/Music"'
+alias unison='unison -ui text'
+alias sync-music='unison music'

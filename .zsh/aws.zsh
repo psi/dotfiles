735c6f61d9266f23f18607762f6969635f25c7c5
JD Harrington <jd@jdharrington.net>
2012-10-22 12:07:16 -0400

    add auto-scaling config for aws

--- .zsh/aws.zsh
+++ .zsh/aws.zsh
@@ -10,6 +10,8 @@ export AWS_SSH_KEY_FILE='~/.ssh/id_jd'
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
 export EC2_CERT=~/.ec2/cert.pem
 
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export JAVA_HOME="$(/usr/libexec/java_home)"
+
 export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
-export AWS_CLOUDFORMATION_HOME="/usr/local/Cellar/aws-cfn-tools/1.0.6/jars"
+export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
+export AWS_AUTO_SCALING_HOME="/usr/local/Library/LinkedKegs/auto-scaling/jars"

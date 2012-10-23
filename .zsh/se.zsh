6d91390e889eb8fe66317192b7b4d7dfa6848271
JD Harrington <jd@jdharrington.net>
2012-10-23 14:50:20 -0400

    se:pc dynamically determines admin node

--- .zsh/se.zsh
+++ .zsh/se.zsh
@@ -1,4 +1,4 @@
 export SE_APP_ROOT="/home/app/app/production/current"
-export SE_ADMIN=$(knife search node 'chef_environment:production AND role:se_admin' | grep FQDN: | awk '{print $2}')
+export SE_ADMIN_NODE=$(knife search node 'chef_environment:production AND role:se_admin' | grep FQDN: | awk '{print $2}')
 
-alias se:pc="ssh -t app@${SE_ADMIN} 'cd $SE_APP_ROOT && ./script/console production'"
+alias se:pc="ssh -t app@${SE_ADMIN_NODE} 'cd $SE_APP_ROOT && ./script/console production'"

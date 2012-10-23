94b6c32f5586dbaf52d7d5f57c0425c092fac2ea
JD Harrington <jd@jdharrington.net>
2012-10-23 14:51:01 -0400

    fix aliasing of chef omnibus binaries

--- .zsh/chef.zsh
+++ .zsh/chef.zsh
@@ -3,10 +3,11 @@ CHEF_PKG_DIR="/opt/chef"
 if [ -d ${CHEF_PKG_DIR} ]; then
   # Always use packaged Chef binaries
   for command in ${CHEF_PKG_DIR}/bin/*; do
-    alias ${command}=${CHEF_PKG_DIR}/bin/${command}
+    command=$(basename ${command})
+    alias ${command}=/usr/bin/${command}
   done
 
-  alias chef-bundle="#{CHEF_PKG_DIR}/embedded/bin/bundle"
+  alias chef-bundle="${CHEF_PKG_DIR}/embedded/bin/bundle"
 fi
 
 alias knuck="knife cookbook upload $@"

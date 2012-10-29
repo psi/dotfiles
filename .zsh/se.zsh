285927551d569d685be574f326d0ebbd44f60df7
JD Harrington <jd@jdharrington.net>
2012-10-29 16:47:30 -0400

    this should totally work here but doesn't. whatever.

--- .zsh/se.zsh
+++
@@ -1,4 +0,0 @@
-export SE_APP_ROOT="/home/app/app/production/current"
-export SE_ADMIN_NODE=$(knife search node 'chef_environment:production AND role:se_admin' | grep FQDN: | awk '{print $2}')
-
-alias se:pc="ssh -t app@${SE_ADMIN_NODE} 'cd $SE_APP_ROOT && ./script/console production'"

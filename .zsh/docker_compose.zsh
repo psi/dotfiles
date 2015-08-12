60bbc5dd5aaefc5ddfdb7b090ba9f968c7e62d01
JD Harrington <jd@jdharrington.net>
2015-08-12 09:56:56 -0400

    docker-compose loads docker env if not present

--- .zsh/docker_compose.zsh
+++ .zsh/docker_compose.zsh
@@ -2,3 +2,14 @@ function drake() {
   service=$(basename ${PWD})
   docker-compose run ${service} rake $@
 }
+
+function _docker-compose() {
+  if [ "$DOCKER_HOST" = "" ]; then
+    echo "Initializing Docker env..."
+    b2d-shell-init
+  fi
+
+  docker-compose $@
+}
+
+alias docker-compose=_docker-compose

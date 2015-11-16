0fcfe56e33c25dbb23b7937ae9ca13d085119356
JD Harrington <jd@jdharrington.net>
2015-11-16 13:30:38 -0500

    Default to us-east-1

--- .zsh/aws.zsh
+++ .zsh/aws.zsh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY=$AMAZON_SECRET_ACCESS_KEY
 export AWS_CREDENTIAL_FILE=$HOME/.aws/credentials
 export AWS_SSH_KEY_ID='jd'
 export AWS_SSH_KEY_FILE='~/.ssh/id_jd'
-export AWS_REGION=us-west-2
+export AWS_REGION=us-east-1
 
 # Completions for awscli
 [ -f /usr/local/share/zsh/site-functions/_aws ] && \

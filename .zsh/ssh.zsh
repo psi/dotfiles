6668221c60fd2ce6a7d96144e81534e65495540c
JD Harrington <jd@jdharrington.net>
2016-07-01 09:03:05 -0400

    Add support for passing username and other options when SSHing by EC2 instance-id

--- .zsh/ssh.zsh
+++ .zsh/ssh.zsh
@@ -1,10 +1,20 @@
 function ssh_ec2_instance_id() {
-  ssh $(ec2_ip_for_instance $1)
+  if [[ "$1" =~ "@" ]]; then
+    user=${1[(ws:@:)1]}
+    host=${1[(ws:@:)2]}
+  else
+    user=$(whoami)
+    host=$1
+  fi
+
+  shift
+
+  ssh ${user}@$(ec2_ip_for_instance $host) $@
 }
 
 function _ssh() {
-  if [[ "$1" =~ "^i-[a-z0-9]{8,17}" ]]; then
-    ssh_ec2_instance_id $1
+  if [[ "$1" =~ "i-[a-z0-9]{8,17}" ]]; then
+    ssh_ec2_instance_id $@
   else
     /usr/bin/ssh $@
   fi

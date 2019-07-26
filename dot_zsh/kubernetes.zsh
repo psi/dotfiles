9cbd4e4fdb0c6309b9555a92f4036b6777a242ac
JD Harrington <jdharrington@instructure.com>
2019-07-26 13:50:59 -0400

    Add a few k8s niceties

--- dot_zsh/kubernetes.zsh
+++ dot_zsh/kubernetes.zsh
@@ -1,3 +1,7 @@
+eval "$(kubectl completion zsh)"
+
+alias k='kubectl'
+
 function kube-decode-secret() {
   secret=$1
   namespace=${2:=default}
@@ -5,4 +9,23 @@ function kube-decode-secret() {
   kubectl get -n ${namespace} secrets/${secret} -o yaml \
     | yaml2json \
     | jq '.data | to_entries | map( {(.key) : (.value | @base64d) } ) | add'
+}
+
+# Stolen from https://www.nrmitchi.com/2019/01/managing-kubeconfig-files/
+function set-kubeconfig {
+  # Sets the KUBECONFIG environment variable to a dynamic concatentation of everything
+  # under ~/.kube/conf.d/*
+  # Does NOT overwrite KUBECONFIG if the KUBECONFIG_MANUAL env var is set
+
+  if [ -d ~/.kube/conf.d ]; then
+    if [ -z "$KUBECONFIG_MANUAL" ]; then
+      export KUBECONFIG=~/.kube/config$(find ~/.kube/conf.d -type f 2>/dev/null | xargs -I % echo -n ":%")
+    fi
+  fi
+}
+
+add-zsh-hook precmd set-kubeconfig
+
+function kube-shell() {
+  kubectl run --generator=run-pod/v1 shell-$(whoami)-$(random-id) --rm -i --tty --image nicolaka/netshoot -n $1 -- /bin/bash
 }

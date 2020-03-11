ddd094e1c5dd191341a34dedb6990f5d54961c03
JD Harrington <jdharrington@instructure.com>
2020-03-11 15:56:14 -0400

    Some kube updates

--- dot_zsh/kubernetes.zsh
+++ dot_zsh/kubernetes.zsh
@@ -22,12 +22,26 @@ function set-kubeconfig {
       export KUBECONFIG=~/.kube/config$(find ~/.kube/conf.d -type f 2>/dev/null | xargs -I % echo -n ":%")
     fi
   fi
+
+  if [ -d ~/.config/k3d ]; then
+    # Force writing of kubeconfigs for newly created clusters
+    # TODO: This only needs to run when I create a cluster
+    k3d get-kubeconfig --all &>/dev/null
+
+    if [ -z "$KUBECONFIG_MANUAL" ]; then
+      export KUBECONFIG=${KUBECONFIG}$(find ~/.config/k3d -type f -name 'kubeconfig.yaml' 2>/dev/null | xargs -I % echo -n ":%")
+    fi
+  fi
 }
 
 add-zsh-hook precmd set-kubeconfig
 
+function kube-get-context() {
+  echo "$(kubectx -c)/$(kubens -c)"
+}
+
 function kube-shell() {
-  kubectl run --generator=run-pod/v1 shell-$(whoami)-$(random-id) --labels app=netshoot --rm -i --tty --image jdharrington/toolbox $@ -- /bin/bash
+  kubectl run --generator=run-pod/v1 shell-$(whoami)-$(random-id) --image-pull-policy="Always" --labels app=toolbox --rm -i --tty --image jdharrington/toolbox $@ -- /bin/bash
 }
 
 # This lets octant work with my munged together KUBECONFIG env variable
